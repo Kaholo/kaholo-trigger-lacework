aab6a96d40cab39b42ad81a75fa17ccf4d2fb8c7
Amit Alon <amit@kaholo.io>
2021-06-27 12:05:34 +0300

    refactored to new format and fixed severity checking

--- src/app.js
+++ src/app.js
@@ -1,44 +1,44 @@
-const { findTriggers } = require("./helpers");
+const translateMap = {"Any": "any", "Info": 5, "Low": 4, 
+                      "Medium": 3, "High": 2, "Critical": 1,
+                      "5": 5, "4": 4, "3": 3, "2": 2, "1": 1};
 
-function alertWebhook(req, res) {
-    const body = req.body;
+async function alertWebhook(req, res, settings, triggerControllers) {
+  if (!triggerControllers) {
+    return res.status(400).send("triggers cannot be nil");
+  }
+  const body = req.body;
+  try {
+    const reqEType = body.event_type; // Get event type
+    const reqEDescription = body.event_description; // Get event ID
+    const reqESeverity = body.event_severity; // Get event severity 
+    const reqETitle = body.event_title; // Get event severity
+    if (!reqEType || !reqEDescription || !reqESeverity){
+      res.status(400).send("bad lacework alert format");
+      console.error("bad lacework alert format");
+    }
+    triggerControllers.forEach(trigger => {
+      let {eventType, id, eventSeverity, includeHigherSev} = trigger.params;
+      if (typeof(eventSeverity) === "string"){
+        eventSeverity = translateMap[eventSeverity] || "any";
+        if (eventSeverity === "any") {
+          includeHigherSev = true, eventSeverity = 5;
+        }
+      }
+      eventType = eventType || "Any";
+      
+      if (eventType !== "Any" && reqEType !== eventType) return;
+      if (reqESeverity > eventSeverity) return;
+      if (!includeHigherSev && reqESeverity !== eventSeverity) return;
+      if (id && !reqEDescription.includes(` ${triggerEventId} `)) return;
 
-    const eventType = body.event_type; // Get event type
-    const eventDescription = body.event_description; // Get event ID
-    const eventSeverity = body.event_severity; // Get event severity 
-    const eventTitle = body.event_title; // Get event severity
-    if (!eventType || !eventDescription || !eventSeverity){
-        throw "bad lacework alert format";
-    }
-
-    findTriggers(
-      validateTrigger,
-      { eventType, eventDescription, eventSeverity },
-      req, res, "alertWebhook",
-      `${eventTitle} - severity ${eventSeverity}` // event description for kaholo
-    );
-}
-  
-async function validateTrigger(trigger, { eventType, eventDescription, eventSeverity }) {
-    const triggerEventType = trigger.params.find((o) => o.name === "eventType").value || "any";
-    const triggerEventId = (trigger.params.find((o) => o.name === "id").value || "").trim();
-    const triggerEventSeverity = trigger.params.find((o) => o.name === "eventSeverity").value || "any";
-    /**
-     * if event type was provided check it matches event type in post request
-     */
-    if (triggerEventType !== "any" && eventType !== triggerEventType) {
-      throw "Not same event type";
-    }
-    /**
-     * if event ID was provided check it matches event ID in post request
-     */
-    if (triggerEventId && !eventDescription.includes(` ${triggerEventId} `)){
-        throw "Not same event ID";;
-    }
-    if (triggerEventSeverity != "any" && eventSeverity != triggerEventSeverity){
-      throw "Not same event severity";
-    }
-    return true;
+      const msg = `${reqETitle} - severity ${reqESeverity}`;
+      trigger.execute(msg, body);
+    });
+    res.status(200).send("OK");
+  }
+  catch (err){
+    res.status(422).send(err.message);
+  }
 }
   
 module.exports = { alertWebhook }
