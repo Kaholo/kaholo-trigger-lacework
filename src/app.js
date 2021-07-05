3d47a5c126051e58399e2c5f1d3ddb635c58d097
Amit Alon <amit@kaholo.io>
2021-07-05 15:02:26 +0300

    updated message

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-const translateMap = {"Any": "any", "Info": 5, "Low": 4, 
+const translateTextMap = {"Any": "any", "Info": 5, "Low": 4, 
                       "Medium": 3, "High": 2, "Critical": 1,
                       "5": 5, "4": 4, "3": 3, "2": 2, "1": 1};
+const translateNumMap = {0: "", 1: "- Critical", 2: "- High Severity", 3: "- Medium Severity", 4: "- Low Severity", 5: "- Info"};
 
 async function alertWebhook(req, res, settings, triggerControllers) {
   const body = req.body;
@@ -17,7 +18,7 @@ async function alertWebhook(req, res, settings, triggerControllers) {
     triggerControllers.forEach(trigger => {
       let {eventType, id, eventSeverity, includeHigherSev} = trigger.params;
       if (typeof(eventSeverity) === "string"){
-        eventSeverity = translateMap[eventSeverity] || "any";
+        eventSeverity = translateTextMap[eventSeverity] || "any";
         if (eventSeverity === "any") {
           includeHigherSev = true, eventSeverity = 5;
         }
@@ -29,7 +30,7 @@ async function alertWebhook(req, res, settings, triggerControllers) {
       if (!includeHigherSev && reqESeverity !== eventSeverity) return;
       if (id && recId !== id) return;
 
-      const msg = `${reqETitle} - severity ${reqESeverity}`;
+      const msg = `${reqETitle} ${translateNumMap[reqESeverity]}`;
       trigger.execute(msg, body);
     });
     res.status(200).send("OK");
